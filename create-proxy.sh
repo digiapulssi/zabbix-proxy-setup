6642b16922e4d16a7305f361e3bc904e00c5bef3
ilniemi <58262535+ilniemi@users.noreply.github.com>
2021-10-19 11:12:57 +0300

    Update create-proxy.sh
    Update so that the comparison is made for the whole name of the container, not for substrings.

--- create-proxy.sh
+++ create-proxy.sh
@@ -25,13 +25,13 @@ KEY_FILE=zabbix/enc/zabbix_proxy_key.pem
 CA_FILE=zabbix/enc/zabbix_proxy.ca
 START_CMD="docker-entrypoint.sh"
 
-if [ "$(docker ps -aq -f name=${NAME})" ]; then
+if [ "$(docker ps -a -f name=${NAME} | awk '{print $NF}' | grep -e ^${NAME}$)" ]; then
   echo "Container with name '${NAME}' already exists. Stop and remove old container before creating new one."
   exit 1
-elif [ "$(docker ps -aq -f name="zabbix-java-gateway")" ]; then
+elif [ "$(docker ps -a -f name=zabbix-java-gateway | awk '{print $NF}' | grep -e ^zabbix-java-gateway$)" ]; then
   echo "Container with name 'zabbix-java-gateway' already exists. Stop and remove old container before creating new one."
   exit 1
-elif [ "$(docker ps -aq -f name="zabbix-snmptraps")" ]; then
+elif [ "$(docker ps -a -f name=zabbix-snmptraps | awk '{print $NF}' | grep -e ^zabbix-snmptraps$)" ]; then
   echo "Container with name 'zabbix-snmptraps' already exists. Stop and remove old container before creating new one."
   exit 1
 fi
